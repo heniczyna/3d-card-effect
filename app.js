0be94ed0e7777d6cb7bae1c25093f395aaf9e0ef
MC <chelmikk@o2.pl>
2020-11-05 23:46:03 +0100

    Improve transition when animation starts

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const description = document.querySelector(".info h3");
 
 //Moving Animation Event
 container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / (-25);
   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 //Animate In
 container.addEventListener("mouseenter", (e) => {
-  card.style.transition = "none";
+  card.style.transition = "all 0.1s ease";
   //Popout
   title.style.transform = "translateZ(150px)";
   logo.style.transform = "translateZ(200px) rotateZ(-45deg)";
